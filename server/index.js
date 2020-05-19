857492b44b17ad5bb78e17fb554dab107f7871e6
tksilicon <frankgod02@gmail.com>
2020-05-19 17:28:33 +0100

    Setup and Added a Procfile.

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.json());
 
 app.use(express.static('dist'));
 
 //app.use('/', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'dist/index.html')));
 
 app.use('/', (req, res) => res.redirect(path.resolve(__dirname, '..', 'dist/index.html')));
+
+app.get('/api/test' (req, res)=>{
+  return res.status(200).json({"test":"test"});
+});
 
 const PORT = process.env.PORT || 3000;
 
