953022d6a3ca48efdbfe132f8a0e5f10b52f3294
tksilicon <frankgod02@gmail.com>
2020-05-19 18:03:26 +0100

    set up express server for backend skeleton and heroku deploy

--- server/index.js
+++ server/index.js
@@ -4,22 +4,13 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
-const isProd = process.env.NOD_ENV === 'production';
-
 app.use(express.static('dist'));
 
-//app.use('/', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'dist/index.html')));
-
-
 app.get('/', (req, res) => {
-
   res.sendFile(path.resolve(__dirname, '..', 'dist/index.html'));
 });
 
-
-app.get('/api/test', (req, res)=>{
-  return res.status(200).json({"test":"test"});
-});
+app.get('/api/test', (req, res) => res.status(200).json({ test }));
 
 const PORT = process.env.PORT || 3000;
 
